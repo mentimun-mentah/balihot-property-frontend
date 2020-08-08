07c9a367d2e24b0b0fb7d11f1886ca376b9fc5d4
komangsu <mangokky@gmail.com>
2020-08-08 13:40:34 +0800

    update axios lib

--- dashboard/lib/axios.js
+++ dashboard/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import createAuthRefreshInterceptor from 'axios-auth-refresh';
-import {parseCookies, setCookie} from "nookies";
+import Router from "next/router";
+import {parseCookies, setCookie, destroyCookie} from "nookies";
 
 const instance = axios.create({
   baseURL: process.env.API_URL,
@@ -8,12 +8,8 @@ const instance = axios.create({
 
 const {access_token, refresh_token} = parseCookies()
 
-export const headerRefresh = {
-  headers: { Authorization: `Bearer ${refresh_token}` }
-}
-
 export const headerCfg = {
-  headers: { Authorization: `Bearer ${access_token}`, }
+  headers: { Authorization: `Bearer ${access_token}` }
 }
 
 export const headerCfgFormData = {
@@ -23,19 +19,53 @@ export const headerCfgFormData = {
   }
 }
 
-const refreshAuthLogic = async (failedRequest) => {
-  return instance
-    .post('/refresh', null, headerRefresh)
-    .then((res) => {
-      setCookie(null, "access_token", res.data.access_token, {
-        maxAge: 30 * 24 * 60 * 60,
-        path: "/",
-      });
-      failedRequest.response.config.headers.Authorization = `Bearer ${res.data.access_token}`;
-        return Promise.resolve();
-    });
-};
+instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    // Do something with request error
+    console.log("========== REQUEST ============", error.response)
+    return Promise.reject(error);
+  });
 
-createAuthRefreshInterceptor(instance, refreshAuthLogic);
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    const headerRefresh = {
+      headers: { Authorization: `Bearer ${refresh_token}` }
+    }
+
+    if(error.response.data.msg === "Token has been revoked"){
+      destroyCookie(null, "access_token")
+      destroyCookie(null, "refresh_token")
+      destroyCookie(null, "username")
+      Router.reload("/")
+    }
+
+    if(error.response.data.msg === "Token has expired"){
+      instance.post('/refresh', null, headerRefresh)
+        .then(res => {
+          setCookie(null, "access_token", res.data.access_token, {
+            maxAge: 30 * 24 * 60 * 60,
+            path: "/",
+          })
+          return Promise.resolve()
+        })
+        .catch(err => {
+          if(err.response && err.response.data && err.response.data.msg === "Token has been revoked"){
+            destroyCookie(null, "access_token")
+            destroyCookie(null, "refresh_token")
+            destroyCookie(null, "username")
+            Router.reload("/")
+          }
+        })
+    }
+    return Promise.reject(error);
+  });
 
 export default instance;

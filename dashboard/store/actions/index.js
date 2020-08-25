76699d36cdec2418ad04292b4ec7bd66c6971cd0
komangsu <mangokky@gmail.com>
2020-08-25 15:57:41 +0800

    add pagination in manage property

--- dashboard/store/actions/index.js
+++ dashboard/store/actions/index.js
@@ -1,7 +1,7 @@
 export { getType, getTypeSuccess } from "./types";
 export { getTeam, getTeamSuccess } from "./teams";
 export { getRegion, getRegionSuccess } from "./region";
-export { getProperty, getPropertySuccess } from "./property";
+export { getProperty, getPropertySuccess, getPropertyBy } from "./property";
 export { getFacility, getFacilitySuccess } from "./facilities";
 export { authCheckState, authSuccess, refreshToken, getUserSuccess } from "./auth";
 

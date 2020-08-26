8aac34196394a29476939454194211bc38fe1ebc
komangsu <mangokky@gmail.com>
2020-08-26 13:04:24 +0800

    add copy link

--- home/components/Card/ShareModal.jsx
+++ home/components/Card/ShareModal.jsx
@@ -1,12 +1,28 @@
+import { useState, useCallback } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
-import { EmailShareButton, FacebookShareButton, LineShareButton } from "react-share";
+import { EmailShareButton, FacebookShareButton } from "react-share";
 import { TelegramShareButton, TwitterShareButton, WhatsappShareButton, } from "react-share";
-import { EmailIcon, FacebookIcon, LineIcon, TelegramIcon, TwitterIcon, WhatsappIcon, } from "react-share";
+import { EmailIcon, FacebookIcon, TelegramIcon, TwitterIcon, WhatsappIcon, } from "react-share";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const ShareModal = ({ link }) => {
+  const [copy, setCopy] = useState('Copy Link')
+
+  const copyHandler = useCallback(() => {
+    setCopy('Link Copied!')
+    setTimeout(() => {
+      setCopy('Copy Link')
+    }, 4000)
+  },[])
+
   return (
     <>
       <ListGroup variant="flush">
+        <CopyToClipboard onCopy={copyHandler} text={link} >
+          <ListGroup.Item className="share-button border-0 hov_pointer">
+            <i className="fas fa-copy fa-fw ic-copy"></i> <span className="ml-2 fs-14 fw-500">{copy}</span>
+          </ListGroup.Item>
+        </CopyToClipboard>
         <EmailShareButton url={link} className="w-100 text-left share-button">
           <ListGroup.Item className="share-button border-0">
             <EmailIcon size={32} /> <span className="ml-2 fs-14 fw-500">Share on Email</span>
@@ -22,11 +38,6 @@ const ShareModal = ({ link }) => {
             <TwitterIcon size={32} /> <span className="ml-2 fs-14 fw-500">Share on Twitter</span>
           </ListGroup.Item>
         </TwitterShareButton>
-        <LineShareButton url={link} className="w-100 text-left share-button">
-          <ListGroup.Item className="share-button border-0">
-            <LineIcon size={32} /> <span className="ml-2 fs-14 fw-500">Share on Line</span>
-          </ListGroup.Item>
-        </LineShareButton>
         <WhatsappShareButton url={link} className="w-100 text-left share-button">
           <ListGroup.Item className="share-button border-0">
             <WhatsappIcon size={32} /> <span className="ml-2 fs-14 fw-500">Share on Whatsapp</span>
@@ -44,6 +55,17 @@ const ShareModal = ({ link }) => {
         }
         :global(.share-button:focus){
           background-color: #efefef;
+        }
+        .ic-copy{
+          width: 32px;
+          height: 32px;
+          font-size: 18px;
+          text-align: center;
+          vertical-align: middle;
+          display: inline-block;
+          background: steelblue;
+          line-height: 1.7;
+          color: white;
         }
       `}</style>
     </>
